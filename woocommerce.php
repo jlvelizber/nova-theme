c7e4a2f4624c93ec921b017e621d4eaf14bb30c9
Jorge Veliz <jorgeconsalvacion@gmail.com>
2026-05-13 15:58:16 -0500

    remove titlte

--- woocommerce.php
+++ woocommerce.php
@@ -9,7 +9,9 @@ get_header();
 ?>
 
 <main id="primary" class="site-main site-container">
-
+	<?php woocommerce_content(
+		array('title' => false) // Hide the page title and breadcrumb on product pages
+	); ?>
 </main>
 
 <?php
